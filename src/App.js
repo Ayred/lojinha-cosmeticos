bfa916696c9b2474bbb211cc9a2f4579b82813d2
Ayred Antara Caicedo Torres <ayredantara@gmail.com>
2022-12-05 15:33:17 -0800

    Logo and menu created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header/Header.jsx';
+import Slider from './components/slider/Slider';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Slider />
     </div>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
+import { CgShoppingBag } from 'react-icons/cg'
+import logo from '../../assets/logo.png'
 import css from './Header.module.css'
 
 const header = () => {
   return (
     <div className={css.container}>
       <div className={css.logo}>
-        Logo
+        <img src={logo} alt="Logo da Lojinha de cosméticos" />
+      
+        <span>Lojinha de Cosméticos</span>
       </div>
       <div className={css.right}>
         <div className={css.menu}>
-
+          <ul className={css.menu}>
+            <li>Quem Somos</li>
+            <li>Nossos Produtos</li>
+            <li>Novos</li>
+            <li>PT-BR</li>
+          </ul>
         </div>
-        <input type="text" className={css.search}/>
-        <span>Fotica de Icon</span>
+     
+        <CgShoppingBag className={css.cart}/>
       </div>
     </div>
   )
